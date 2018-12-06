3c2c68f8224d2c4e361157b9bffb7ed033d32535
Dylan Cavanaugh <cavanadm@gmail.com>
2018-12-06 14:02:53 -0600

    Initial commit

---
+++ MDITextEditor/MDIEditor.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace MDITextEditor
+{
+    partial class MDIEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.size8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.size12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.size15 = new System.Windows.Forms.ToolStripMenuItem();
+            this.size18 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timesNewRomanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.couriorNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.windowToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MdiWindowListItem = this.windowToolStripMenuItem;
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(418, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "F&ormat";
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.size8,
+            this.size12,
+            this.size15,
+            this.size18});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // size8
+            // 
+            this.size8.CheckOnClick = true;
+            this.size8.Name = "size8";
+            this.size8.Size = new System.Drawing.Size(86, 22);
+            this.size8.Text = "8";
+            this.size8.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // size12
+            // 
+            this.size12.CheckOnClick = true;
+            this.size12.Name = "size12";
+            this.size12.Size = new System.Drawing.Size(86, 22);
+            this.size12.Text = "12";
+            this.size12.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // size15
+            // 
+            this.size15.CheckOnClick = true;
+            this.size15.Name = "size15";
+            this.size15.Size = new System.Drawing.Size(86, 22);
+            this.size15.Text = "15";
+            this.size15.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // size18
+            // 
+            this.size18.CheckOnClick = true;
+            this.size18.Name = "size18";
+            this.size18.Size = new System.Drawing.Size(86, 22);
+            this.size18.Text = "18";
+            this.size18.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timesNewRomanToolStripMenuItem,
+            this.arialToolStripMenuItem,
+            this.couriorNewToolStripMenuItem});
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            // 
+            // timesNewRomanToolStripMenuItem
+            // 
+            this.timesNewRomanToolStripMenuItem.Name = "timesNewRomanToolStripMenuItem";
+            this.timesNewRomanToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.timesNewRomanToolStripMenuItem.Text = "Times New Roman";
+            this.timesNewRomanToolStripMenuItem.Click += new System.EventHandler(this.timesNewRomanToolStripMenuItem_Click);
+            // 
+            // arialToolStripMenuItem
+            // 
+            this.arialToolStripMenuItem.Name = "arialToolStripMenuItem";
+            this.arialToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.arialToolStripMenuItem.Text = "Arial";
+            this.arialToolStripMenuItem.Click += new System.EventHandler(this.arialToolStripMenuItem_Click);
+            // 
+            // couriorNewToolStripMenuItem
+            // 
+            this.couriorNewToolStripMenuItem.Name = "couriorNewToolStripMenuItem";
+            this.couriorNewToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.couriorNewToolStripMenuItem.Text = "Courior New";
+            this.couriorNewToolStripMenuItem.Click += new System.EventHandler(this.couriorNewToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blackToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.redToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.CheckOnClick = true;
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blackToolStripMenuItem.Text = "Black";
+            this.blackToolStripMenuItem.Click += new System.EventHandler(this.blackToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.CheckOnClick = true;
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.CheckOnClick = true;
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.CheckOnClick = true;
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cascadeToolStripMenuItem,
+            this.verticalToolStripMenuItem,
+            this.horizontalToolStripMenuItem});
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.windowToolStripMenuItem.Text = "&Window";
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.cascadeToolStripMenuItem_Click);
+            // 
+            // verticalToolStripMenuItem
+            // 
+            this.verticalToolStripMenuItem.Name = "verticalToolStripMenuItem";
+            this.verticalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.verticalToolStripMenuItem.Text = "Vertical";
+            this.verticalToolStripMenuItem.Click += new System.EventHandler(this.verticalToolStripMenuItem_Click);
+            // 
+            // horizontalToolStripMenuItem
+            // 
+            this.horizontalToolStripMenuItem.Name = "horizontalToolStripMenuItem";
+            this.horizontalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.horizontalToolStripMenuItem.Text = "Horizontal";
+            this.horizontalToolStripMenuItem.Click += new System.EventHandler(this.horizontalToolStripMenuItem_Click);
+            // 
+            // MDIEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 298);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MDIEditor";
+            this.Text = "MDI Text Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem size8;
+        private System.Windows.Forms.ToolStripMenuItem size12;
+        private System.Windows.Forms.ToolStripMenuItem size15;
+        private System.Windows.Forms.ToolStripMenuItem size18;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timesNewRomanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem couriorNewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horizontalToolStripMenuItem;
+    }
+}
+
---
+++ MDITextEditor/MDIEditor.cs
@@ -0,0 +1,304 @@
+﻿//Program By : Dylan Cavanaugh
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDITextEditor
+{
+    public partial class MDIEditor : Form
+    {
+        //This variable will be used to number the files
+        int counter = 0;
+
+        public MDIEditor()
+        {
+            InitializeComponent();
+        }
+
+        //This is the "new" click event handler
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Creates new child form
+            var child = new ChildForm();
+            child.MdiParent = this;
+
+            //Utilizes the counter to number all text files
+            if (counter == 0)
+            {
+                child.Text = "Text File ";
+            }
+            else
+            {
+                child.Text = "Text File " + counter;
+            }
+            counter++;
+            child.Show();
+
+        }
+
+        //Closes the current active child
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try {
+                ActiveMdiChild.Close();
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Exits the application
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        //Changes the font size to 8
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 8);
+            size12.Checked = false;
+            size15.Checked = false;
+            size18.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font size to 12
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 12);
+            size8.Checked = false;
+            size15.Checked = false;
+            size18.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font size to 15
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 15);
+            size8.Checked = false;
+            size12.Checked = false;
+            size18.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font size to 18
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            try {
+            ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 18);
+            size8.Checked = false;
+            size12.Checked = false;
+            size15.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font to Times New Roman
+        //Changing the font also changed the size of the text
+        //so an if statement was used to ensure the size was unchanged
+        private void timesNewRomanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Font = new Font("Times New Roman", 8, ActiveMdiChild.Font.Style);
+
+            if (size8.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 8);
+            }
+            else if (size12.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 12);
+            }
+            else if (size15.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 15);
+            }
+            else if (size18.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 18);
+            }
+            timesNewRomanToolStripMenuItem.Checked = true;
+            arialToolStripMenuItem.Checked = false;
+            couriorNewToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font to Arial
+        private void arialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Font = new Font("Arial", 8, ActiveMdiChild.Font.Style);
+
+            if (size8.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 8);
+            }
+            else if (size12.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 12);
+            }
+            else if (size15.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 15);
+            }
+            else if (size18.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 18);
+            }
+            timesNewRomanToolStripMenuItem.Checked = false;
+            arialToolStripMenuItem.Checked = true;
+            couriorNewToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes the font to Courior New
+        private void couriorNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Font = new Font("Courior New", 8, ActiveMdiChild.Font.Style);
+
+            if (size8.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 8);
+            }
+            else if (size12.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 12);
+            }
+            else if (size15.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 15);
+            }
+            else if (size18.Checked)
+            {
+                ActiveMdiChild.Controls[0].Font = new Font(ActiveMdiChild.Font.Name, 18);
+            }
+            timesNewRomanToolStripMenuItem.Checked = false;
+            arialToolStripMenuItem.Checked = false;
+            couriorNewToolStripMenuItem.Checked = true;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+
+        }
+
+        //Changes font color to black
+        private void blackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].ForeColor = Color.Black;
+            blueToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = false;
+            redToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes font color to blue
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].ForeColor = Color.Blue;
+            blackToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = false;
+            redToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes font color to green
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].ForeColor = Color.Green;
+            blueToolStripMenuItem.Checked = false;
+            blackToolStripMenuItem.Checked = false;
+            redToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Changes font color to red
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            ActiveMdiChild.Controls[0].ForeColor = Color.Red;
+            blueToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = false;
+            blackToolStripMenuItem.Checked = false;
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("There are no files open!");
+            }
+        }
+
+        //Cascades all the windows
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        //Arranges the windows vertically
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        //Arranges the windows horizontally
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+    }
+}
